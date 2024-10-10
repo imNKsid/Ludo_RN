321a32272fed55149d414072540f8cd13f0c5fdf
Nitesh Kumar <imnksid@gmail.com>
2024-10-10 18:42:44 +0530

    Exporting all the components

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+import CustomButton from "./custom-button";
+import NewGameModal from "./new-game-modal";
+import PlayerField from "./player-field";
+
+export { CustomButton, NewGameModal, PlayerField };
