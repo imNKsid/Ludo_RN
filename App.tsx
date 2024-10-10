44c62c37acb47b628e5226746d08db2f3564c4ac
Nitesh Kumar <imnksid@gmail.com>
2024-10-10 18:48:29 +0530

    Added gameInProgress, showStartModal, different players variable & passing to Home

--- App.tsx
+++ App.tsx
@@ -1,13 +1,44 @@
 import { View, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { Game, Home } from "./src/screens";
 
 const App = () => {
-  const isGameInProgress = false;
+  const [gameInProgress, setGameInProgress] = useState(false);
+  const [showStartModal, setShowStartModal] = useState(false);
+  const [redPlayer, setRedPlayer] = useState({ name: "" });
+  const [yellowPlayer, setYellowPlayer] = useState({ name: "" });
+  const [greenPlayer, setGreenPlayer] = useState({ name: "" });
+  const [bluePlayer, setBluePlayer] = useState({ name: "" });
 
   return (
     <View style={styles.container}>
-      <>{isGameInProgress ? <Game /> : <Home />}</>
+      <>
+        {gameInProgress ? (
+          <Game />
+        ) : (
+          <Home
+            showStartGameModal={showStartModal}
+            onNewGameBtnPress={() => setShowStartModal(true)}
+            onCancel={() => setShowStartModal(false)}
+            playerRed={redPlayer}
+            playerYellow={yellowPlayer}
+            playerGreen={greenPlayer}
+            playerBlue={bluePlayer}
+            onRedInput={(text: any) =>
+              setRedPlayer({ ...redPlayer, name: text })
+            }
+            onYellowInput={(text: any) =>
+              setYellowPlayer({ ...yellowPlayer, name: text })
+            }
+            onGreenInput={(text: any) =>
+              setGreenPlayer({ ...greenPlayer, name: text })
+            }
+            onBlueInput={(text: any) =>
+              setBluePlayer({ ...bluePlayer, name: text })
+            }
+          />
+        )}
+      </>
     </View>
   );
 };
