7cb92506491d1a3b313877448f8e30ad20cc402c
Nitesh Kumar <imnksid@gmail.com>
2024-10-10 08:53:37 +0530

    Added a header, a btn & an image in Home

--- src/screens/home-screen.tsx
+++ src/screens/home-screen.tsx
@@ -1,14 +1,48 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import { IMAGES } from "../assets";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.logoStyle}>Ludo Classic</Text>
+      <TouchableOpacity style={styles.newBtn}>
+        <Text>New Game</Text>
+      </TouchableOpacity>
+      <Image source={IMAGES.board} style={styles.ludoBoard} />
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FF0",
+  },
+  logoStyle: {
+    color: "#F00",
+    fontSize: 40,
+    alignSelf: "center",
+    marginTop: 100,
+    fontWeight: "bold",
+  },
+  newBtn: {
+    backgroundColor: "#FFF",
+    width: 200,
+    padding: 10,
+    borderColor: "#DDD",
+    borderWidth: 2,
+    alignSelf: "center",
+    alignItems: "center",
+    marginTop: 20,
+    borderRadius: 10,
+  },
+  ludoBoard: {
+    width: "70%",
+    height: "70%",
+    resizeMode: "contain",
+    alignSelf: "center",
+  },
+});
