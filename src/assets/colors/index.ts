5a9027ed43d92b56a7032d31feb83cc39a455941
Nitesh Kumar <imnksid@gmail.com>
2024-10-15 15:31:20 +0530

    Created COLORS file

---
+++ src/assets/colors/index.ts
@@ -0,0 +1,15 @@
+export const COLORS = {
+  bgColor: "#FF0",
+  redPlayerColor: "#F00",
+  redFieldBorder: "#FFCCCC",
+  redField: "#FFEFEF",
+  yellowPlayerColor: "#DBBA23",
+  yellowFieldBorder: "#FFD520",
+  yellowField: "#FFD",
+  greenPlayerColor: "#27CF27",
+  greenFieldBorder: "#68F768",
+  greenField: "#DFD",
+  bluePlayerColor: "#00F",
+  blueFieldBorder: "#4E4EF5",
+  blueField: "#DDF",
+};
