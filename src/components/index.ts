212d57ced26fdcf5b82d94234dd09a1b036e3440
Nitesh Kumar <imnksid@gmail.com>
2024-10-15 15:31:48 +0530

    Exporting PlayerBox component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import CustomButton from "./custom-button";
 import NewGameModal from "./new-game-modal";
 import PlayerField from "./player-field";
+import PlayerBox from "./player-box";
 
-export { CustomButton, NewGameModal, PlayerField };
+export { CustomButton, NewGameModal, PlayerField, PlayerBox };
