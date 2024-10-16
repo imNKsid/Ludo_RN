71d55f5950313831c842516039c2dcc3ec9588d5
Nitesh Kumar <imnksid@gmail.com>
2024-10-16 21:09:18 +0530

    Added all the 4 pieces inside the PlayerBox

--- src/components/player-box.tsx
+++ src/components/player-box.tsx
@@ -1,9 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
+import { COLORS } from "../assets";
 
 interface PlayerProps {
   color: string;
   name: string;
+  position: string;
 }
 
 interface PlayerBoxProps {
@@ -20,16 +22,56 @@ const PlayerBox = (props: PlayerBoxProps) => {
 
   return (
     <View style={[styles.player, customStyle, { backgroundColor: colorName }]}>
-      <View style={styles.innerContainer}></View>
+      <View style={styles.innerContainer}>
+        <View style={styles.piecesContainer}>
+          <Player piece={one} />
+          <Player piece={two} />
+        </View>
+        <View style={styles.piecesContainer}>
+          <Player piece={three} />
+          <Player piece={four} />
+        </View>
+      </View>
     </View>
   );
 };
 
 export default PlayerBox;
 
+const Player = ({ piece }: { piece: PlayerProps }) => {
+  const { color, position } = piece;
+
+  return (
+    <TouchableOpacity style={{ flex: 1 }}>
+      <View
+        style={[
+          styles.pieceStyle,
+          { backgroundColor: position === "home" ? color : COLORS.white },
+        ]}
+      />
+    </TouchableOpacity>
+  );
+};
+
 const styles = StyleSheet.create({
   player: {
     flex: 3,
   },
-  innerContainer: {},
+  innerContainer: {
+    flex: 1,
+    backgroundColor: COLORS.white,
+    margin: 20,
+    borderRadius: 20,
+  },
+  piecesContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  pieceStyle: {
+    flex: 1,
+    margin: 5,
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: COLORS.black,
+  },
 });
