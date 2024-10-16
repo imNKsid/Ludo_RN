992c1173d62ef3d7207be531d88c41e43dc51e95
Nitesh Kumar <imnksid@gmail.com>
2024-10-16 20:36:13 +0530

    Added 4 boxes & initialized all 4 players

--- src/screens/game-screen.tsx
+++ src/screens/game-screen.tsx
@@ -1,14 +1,117 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { PLAYER, PLAYER_COUNT, WindowDimensions } from "../utils";
+import { PlayerBox } from "../components";
+import { COLORS } from "../assets";
 
 const Game = () => {
+  const { RED, YELLOW, GREEN, BLUE } = PLAYER;
+
+  const { ONE, TWO, THREE, FOUR } = PLAYER_COUNT;
+
+  const {
+    redPlayerColor,
+    yellowPlayerColor,
+    greenPlayerColor,
+    bluePlayerColor,
+  } = COLORS;
+
+  const [red, setRed] = useState({});
+  const [yellow, setYellow] = useState({});
+  const [green, setGreen] = useState({});
+  const [blue, setBlue] = useState({});
+
+  useEffect(() => {
+    const redPlayer = initPlayer(RED, redPlayerColor);
+    setRed(redPlayer);
+    const yellowPlayer = initPlayer(YELLOW, yellowPlayerColor);
+    setYellow(yellowPlayer);
+    const greenPlayer = initPlayer(GREEN, greenPlayerColor);
+    setGreen(greenPlayer);
+    const bluePlayer = initPlayer(BLUE, bluePlayerColor);
+    setBlue(bluePlayer);
+  }, []);
+
+  const initPlayer = (playerType: string, color: string) => {
+    return {
+      pieces: initPieces(color),
+      color,
+      player: playerType,
+    };
+  };
+
+  const initPieces = (playerColor: string) => {
+    return {
+      one: { name: ONE, color: playerColor },
+      two: { name: TWO, color: playerColor },
+      three: { name: THREE, color: playerColor },
+      four: { name: FOUR, color: playerColor },
+    };
+  };
+
   return (
-    <View>
-      <Text>Game</Text>
+    <View style={styles.container}>
+      <View style={styles.gameContainer}>
+        <View style={styles.twoPlayersContainer}>
+          <RenderPlayer player={red} customStyle={styles.redBox} />
+          <View style={{ flex: 2 }} />
+          <RenderPlayer player={yellow} customStyle={styles.yellowBox} />
+        </View>
+        <View style={styles.horizontalCellsContainer} />
+        <View style={styles.twoPlayersContainer}>
+          <RenderPlayer player={green} customStyle={styles.greenBox} />
+          <View style={{ flex: 2 }} />
+          <RenderPlayer player={blue} customStyle={styles.blueBox} />
+        </View>
+      </View>
     </View>
   );
 };
 
 export default Game;
 
-const styles = StyleSheet.create({});
+const RenderPlayer = (props: any) => {
+  const { player, customStyle } = props;
+  const { color, pieces } = player;
+  const { one, two, three, four } = pieces;
+
+  return (
+    <PlayerBox
+      colorName={color}
+      one={one}
+      two={two}
+      three={three}
+      four={four}
+      customStyle={customStyle}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.bgColor,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  gameContainer: {
+    borderWidth: 2,
+    borderRadius: 20,
+    backgroundColor: COLORS.white,
+    width: WindowDimensions.WIDTH,
+    height: WindowDimensions.WIDTH,
+  },
+  twoPlayersContainer: {
+    flex: 3,
+    // backgroundColor: "#DDD",
+    flexDirection: "row",
+  },
+  horizontalCellsContainer: {
+    flex: 2,
+    backgroundColor: COLORS.white,
+  },
+  redBox: { borderTopLeftRadius: 20 },
+  yellowBox: { borderTopRightRadius: 20 },
+  greenBox: { borderBottomLeftRadius: 20 },
+  blueBox: { borderBottomRightRadius: 20 },
+});
