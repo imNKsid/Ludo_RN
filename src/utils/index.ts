c73a9e42473d078dd03b4c186ab00ddf734e2a0e
Nitesh Kumar <imnksid@gmail.com>
2024-10-17 04:19:28 +0530

    Exporting RED_CELLS, YELLOW_CELLS, GREEN_CELLS, BLUE_CELLS from utils

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,23 @@
-import { PLAYER, PLAYER_COUNT, POSITION } from "./app-constants";
+import {
+  PLAYER,
+  PLAYER_COUNT,
+  POSITION,
+  RED_CELLS,
+  YELLOW_CELLS,
+  GREEN_CELLS,
+  BLUE_CELLS,
+} from "./app-constants";
 import { WindowDimensions, isAndroid, isIOS } from "./global-constants";
 
-export { PLAYER, PLAYER_COUNT, POSITION, WindowDimensions, isAndroid, isIOS };
+export {
+  PLAYER,
+  PLAYER_COUNT,
+  POSITION,
+  RED_CELLS,
+  YELLOW_CELLS,
+  GREEN_CELLS,
+  BLUE_CELLS,
+  WindowDimensions,
+  isAndroid,
+  isIOS,
+};
