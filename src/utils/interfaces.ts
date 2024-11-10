01554d5a0664e4778815121307421e9aff743c98
Nitesh Kumar <imnksid@gmail.com>
2024-11-11 01:10:01 +0530

    Created Player, PlayerPieces & PieceProps

---
+++ src/utils/interfaces.ts
@@ -0,0 +1,19 @@
+export interface PlayerProps {
+  pieces: PlayerPiecesProps;
+  color: string;
+  player: string;
+}
+
+export interface PlayerPiecesProps {
+  one: PieceProps;
+  two: PieceProps;
+  three: PieceProps;
+  four: PieceProps;
+}
+
+export interface PieceProps {
+  position: string;
+  name: string;
+  color: string;
+  updateTime: number;
+}
